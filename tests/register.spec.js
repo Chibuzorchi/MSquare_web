672c4e7046a479917555289550b3f8cdfdc9d55e
Chinonso <chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.comgit config --global user.email chinonsochibuzor2277@gmail.com>
2024-06-05 14:05:50 +0100

    Joining process

---
+++ tests/register.spec.js
@@ -0,0 +1,19 @@
+const { test, chromium} = require('@playwright/test');
+
+
+test('click register button', async () => {
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://dev-admin.msq.market/');
+  await page.click('#btn_login_register');
+  await page.getByPlaceholder('Name').fill("Kelly Handsome")
+  await page.getByPlaceholder('Email address').fill("kelly@gmail.com")
+  await page.getByPlaceholder('Password', { exact: true }).fill("123456")
+  await page.getByPlaceholder('Confirm Password', { exact: true }).fill("123456")
+
+  await page.getByRole('checkbox', { name: 'I accept terms & conditions' }).check();
+  
+  await page.getByRole('button', { name: "Sign Up" }).click();
+  
+});
